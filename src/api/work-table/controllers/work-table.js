347cce9d336d9bd3bb01ad7b9e2c5142e02c2bd8
Tor Magne <tormagne-brekken@outlook.com>
2022-06-16 15:22:26 +0200

    init

---
+++ src/api/work-table/controllers/work-table.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  work-table controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::work-table.work-table');
