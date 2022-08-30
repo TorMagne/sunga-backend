9e2ebdf87b0af374b1aaf2da8e6569d245f188da
Tor Magne <tormagne-brekken@outlook.com>
2022-08-30 19:28:15 +0200

    changed back server and db files

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,13 +5,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
-        ca: env("DATABASE_CA"),
+        rejectUnauthorized: false,
       },
     },
     debug: false,
